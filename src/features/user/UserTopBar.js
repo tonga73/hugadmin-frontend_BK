6458d38cfef8b1a1d02e4b886703334d620629dc
gastoire <buzzcutseason73@gmail.com>
2022-09-01 13:17:49 -0300

    fix: Corregida altura de userTopBar

--- src/features/user/UserTopBar.js
+++ src/features/user/UserTopBar.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function UserTopBar() {
-  return <nav className="p-3 text-3xl">User</nav>;
+  return (
+    <div className="navbar gap-x-1.5 bg-base-100 opacity-50 hover:opacity-100">
+      <h3 className="text-3xl">User</h3>
+    </div>
+  );
 }
